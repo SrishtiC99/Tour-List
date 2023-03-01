ad924aca0babbfe3948c284cda819268b792615c
SrishtiC99 <iamsri9631@gmail.com>
2023-03-01 19:55:56 +0530

    tours added

---
+++ src/Tours.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Tour from './Tour';
+function Tours({tours, removeTour}) {
+    return (
+        <section>
+            <h2 className='title'>Our Tours</h2>
+            <div className='underline'></div>
+            <div>
+                {tours.map((tour) => {
+                    return <Tour key={tour.id} {...tour} removeTour={ removeTour} />
+                })}
+            </div>
+        </section>
+    );
+}
+
+export default Tours
